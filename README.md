dc6c3931c38f641424859714ebabd856ec6f27c5
dees040 <d.oomens@hotmail.nl>
2017-08-01 14:54:08 +0200

    Initialized project

---
+++ README.md
@@ -0,0 +1,56 @@
+# Laravel API Responses
+
+A small package for returning an API response easily.
+
+## Usage
+
+Just use one of the helper functions and you're good to go.
+
+```php
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+    /**
+     * Show the given user.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        if (! $user->isAdmin()) {
+            return forbidden();
+        }
+        
+        return ok($user);
+    }
+}
+```
+
+## Methods
+
+All methods accept a `$data` parameter. This can be any data which can be used in a JSON response, such as strings, integers, arrays, models, etc..
+
+| Method                      | Status Code   |
+| --------------------------- |:-------------:|
+| ok($data)                   | 200           |
+| created($data)              | 201           |
+| accepted($data)             | 202           |
+| bad_request($data)          | 400           |
+| unauthenticated($data)      | 401           |
+| forbidden($data)            | 403           |
+| not_found($data)            | 404           |
+| method_not_allowed($data)   | 405           |
+| not_acceptable($data)       | 406           |
+| teapot($data)               | 418           |
+| unprocessable_entity($data) | 422           |
+
+### Custom error code
+
+If you'd wish to send a status code which is not in the list you could use the `json_response($data = null, $status = 200)` helper function.
---
+++ responses.php
@@ -0,0 +1,158 @@
+<?php
+
+if (! function_exists('json_response')) {
+    /**
+     * Return a new JSON response.
+     *
+     * @param  mixed  $data
+     * @param  int  $status
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function json_response($data = null, $status = 200)
+    {
+        return response()->json($data, $status);
+    }
+}
+
+if (! function_exists('ok')) {
+    /**
+     * Return an all OK 200 response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function ok($data = null)
+    {
+        return json_response($data, 200);
+    }
+}
+
+if (! function_exists('created')) {
+    /**
+     * Return created response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function created($data = null)
+    {
+        return json_response($data, 201);
+    }
+}
+
+if (! function_exists('accepted')) {
+    /**
+     * Return accepted response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function accepted($data = null)
+    {
+        return json_response($data, 201);
+    }
+}
+
+if (! function_exists('bad_request')) {
+    /**
+     * Return a bad request response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function bad_request($data = null)
+    {
+        return json_response($data, 400);
+    }
+}
+
+if (! function_exists('unauthenticated')) {
+    /**
+     * Return a unauthenticated response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function unauthenticated($data = null)
+    {
+        return json_response($data, 401);
+    }
+}
+
+if (! function_exists('forbidden')) {
+    /**
+     * Return a forbidden response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function forbidden($data = null)
+    {
+        return json_response($data, 403);
+    }
+}
+
+if (! function_exists('not_found')) {
+    /**
+     * Return a not found response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function not_found($data = null)
+    {
+        return json_response($data, 404);
+    }
+}
+
+if (! function_exists('method_not_allowed')) {
+    /**
+     * Return a method not allowed response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function method_not_allowed($data = null)
+    {
+        return json_response($data, 405);
+    }
+}
+
+if (! function_exists('not_acceptable')) {
+    /**
+     * Return a not acceptable response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function not_acceptable($data = null)
+    {
+        return json_response($data, 406);
+    }
+}
+
+if (! function_exists('teapot')) {
+    /**
+     * All hail the magical teapot response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function unproccessable_entity($data = null)
+    {
+        return json_response($data, 418);
+    }
+}
+
+if (! function_exists('unprocessable_entity')) {
+    /**
+     * Return a unprocessable entity response.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function unprocessable_entity($data = null)
+    {
+        return json_response($data, 422);
+    }
+}
